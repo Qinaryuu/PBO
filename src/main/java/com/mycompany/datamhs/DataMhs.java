bc8e977a849d41f198c28aa39ccba4ef7e3b990a
HP VICTUS <HP VICTUS@LAPTOP-O6I7SH9L>
2026-05-11 10:43:19 +0700

    Reservasi Fasilitas Kampus

---
+++ src/main/java/com/mycompany/datamhs/DataMhs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datamhs;
+
+/**
+ *
+ * @author HP VICTUS
+ */
+public class DataMhs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
